dab07610f125c127bbc472958e26b6734e750125
Janhavi Hivarekar <144115237+Janhavi003@users.noreply.github.com>
2026-01-06 11:19:09 +0530

    Implemented first button and hover effect

--- sketch.js
+++ sketch.js
@@ -4,4 +4,30 @@ function setup() {
 
 function draw() {
   background(20);
+
+  // button size
+  const buttonWidth = 200;
+  const buttonHeight = 100;
+
+  // center position (CORNER mode)
+  const x = width / 2 - buttonWidth / 2;
+  const y = height / 2 - buttonHeight / 2;
+
+  // hover check
+  const isHover =
+    mouseX > x &&
+    mouseX < x + buttonWidth &&
+    mouseY > y &&
+    mouseY < y + buttonHeight;
+
+  // style
+  noStroke();
+  if (isHover) {
+    fill(90, 210, 230);
+  } else {
+    fill(60, 180, 200);
+  }
+
+  // draw button
+  rect(x, y, buttonWidth, buttonHeight, 12);
 }
